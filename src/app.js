f50d6e71f4c45f3812859c4bcff62c7d11e312f3
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-09-25 13:33:12 -0700

    Added hover feature for icon preview

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ const gameBoard = ((size) => {
     }
 })(3);
 
-const playerFactory = (mark, isTurnInitial) => {
+const playerFactory = (mark, isTurnInitial, source) => {
     let isTurn = isTurnInitial;
 
     const swapTurn = () => {
@@ -43,29 +43,31 @@ const playerFactory = (mark, isTurnInitial) => {
         getTurn,
         swapTurn,
         mark,
+        source,
     };
 }
-const user = playerFactory('x', true);
-const computer = playerFactory('o', false);
+const user = playerFactory('x', true, '../icons/x.svg');
+const computer = playerFactory('o', false, '../icons/circle.svg');
 
 const gameController = ((player1, player2, board) => {
     const _playerArray = [player1, player2];
     let _currentPlayer = null;
-    let _otherPlayer = null;
+    let _otherplayer = null;
 
     if (_playerArray[0].getTurn() === true) {
         _currentPlayer = _playerArray[0];
-        _otherPlayer = _playerArray[1];
+        _otherplayer = _playerArray[1];
     }
     else {
         _currentPlayer = _playerArray[1];
-        _otherPlayer = _playerArray[0];
+        _otherplayer = _playerArray[0];
     };
 
-    const getCurrentPlayer = () => console.log(`current player = ${JSON.stringify(_currentPlayer)}`);
+    const getCurrentPlayer = () => _currentPlayer;
+    const getOtherPlayer = () => _otherplayer;
     const _swapPlayers = () => {
-        let hold = _otherPlayer;
-        _otherPlayer = _currentPlayer;
+        let hold = _otherplayer;
+        _otherplayer = _currentPlayer;
         _currentPlayer = hold;
     };
 
@@ -80,7 +82,7 @@ const gameController = ((player1, player2, board) => {
                 return _currentPlayer;
             }
             _currentPlayer.swapTurn();
-            _otherPlayer.swapTurn();
+            _otherplayer.swapTurn();
             _swapPlayers();
             return true;
         }
@@ -119,23 +121,26 @@ const gameController = ((player1, player2, board) => {
 
     return {
         getCurrentPlayer,
+        getOtherPlayer,
         add,
     };
 })(user,computer,gameBoard);
 
-const stringToSourceObjFactory = (string, source) => {
-    return {string, source};
-}
-
-const objectO = stringToSourceObjFactory('o', '../icons/circle.svg');
-const objectX = stringToSourceObjFactory('x', '../icons/x.svg');
-
-const scriptToDOM = ((objO, objX) => {
-    const _insert = (parent, iconSourceString) => {
+const scriptToDOM = ((playerX, playerO) => {
+    const insert = (parent, iconSourceString, isHover = false) => {
+        if (parent.hasChildNodes()) {
+            parent.children[0].classList.remove('faded');
+            return;
+        }
         const image = document.createElement('img');
         image.src = iconSourceString;
+        if (isHover) {
+            image.classList.add('faded');
+        }
         parent.appendChild(image);
     };
+
+
 
     const updateGrid = (parent) => {
         const board = gameBoard.displayBoard();
@@ -145,14 +150,14 @@ const scriptToDOM = ((objO, objX) => {
         for (let i = 0; i < length; i++) {
             currentRow = parent.children[i];
             for (let j = 0; j < length; j++) {
-                if (!currentRow.children[j].hasChildNodes()){
-                    if (board[i][j] == objO.string) {
-                        sourceString = objO.source;
-                        _insert(currentRow.children[j], sourceString);
+                if (!currentRow.children[j].hasChildNodes() || currentRow.children[j].children[0].classList.contains('faded')){
+                    if (board[i][j] == playerO.mark) {
+                        sourceString = playerO.source;
+                        insert(currentRow.children[j], sourceString);
                     }
-                    else if (board[i][j] == objX.string) {
-                        sourceString = objX.source;
-                        _insert(currentRow.children[j], sourceString);
+                    else if (board[i][j] == playerX.mark) {
+                        sourceString = playerX.source;
+                        insert(currentRow.children[j], sourceString);
                     }
                 }
             }
@@ -160,8 +165,9 @@ const scriptToDOM = ((objO, objX) => {
     };
     return {
         updateGrid,
+        insert,
     };
-})(objectO, objectX);
+})(user, computer);
 
 const tttbox = document.getElementById('tttbox');
 const tttbuttonArray = document.querySelectorAll('.tttbutton');
@@ -170,9 +176,18 @@ const tttbuttonArray = document.querySelectorAll('.tttbutton');
 const sideLength = tttbuttonArray.length**0.5;
 for (let i = 0; i < sideLength; i++) {
     for (let j = 0; j < sideLength; j++) {
-        tttbuttonArray[sideLength*i+j].addEventListener("mouseup", (e) => {
+        tttbuttonArray[sideLength*i+j].addEventListener("click", (e) => {
             gameController.add(i,j);
             scriptToDOM.updateGrid(tttbox);
-        } )
+        });
+        tttbuttonArray[sideLength*i+j].addEventListener('mouseenter', (e) => {
+            scriptToDOM.insert(e.target, gameController.getCurrentPlayer().source, true);
+        });
+        tttbuttonArray[sideLength*i+j].addEventListener('mouseleave', (e) => {
+            if (e.target.children[0].classList.contains('faded')) {
+                e.target.children[0].remove();
+            }
+        });
+
     }
 }
